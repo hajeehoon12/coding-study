57ec75c684fcb05284652408202507aaf2672d93
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2022-08-03 08:24:15 +0900

    기본 bfs and dfs

--- DFSBFS/1260(DFS와 BFS).py
+++ DFSBFS/1260(DFS와 BFS).py
@@ -1,3 +1,36 @@
 import sys
+from collections import deque
 
-n, m = map(int, sys.stdin.readline())
+n, m, v = map(int, sys.stdin.readline().split())
+
+graph = [[0] * (n + 1) for _ in range(n + 1)] 
+visit_list = [0] * (n + 1)
+visit_list2 = [0] * (n + 1)
+
+def dfs(v):
+  visit_list2[v] = 1        
+  print(v, end = " ")
+  for i in range(1, n + 1):
+    if visit_list2[i] == 0 and graph[v][i] == 1:
+      dfs(i)
+
+
+def bfs(v):
+  q = deque()
+  q.append(v)       
+  visit_list[v] = 1   
+  while q:
+    t = q.popleft()
+    print(t, end = " ")
+    for i in range(1, n + 1):
+      if visit_list[i] == 0 and graph[t][i] == 1:
+        q.append(i)
+        visit_list[i] = 1
+
+for _ in range(m):
+  a, b = map(int, sys.stdin.readline().split())
+  graph[a][b] = graph[b][a] = 1
+
+dfs(v)
+print()
+bfs(v)
