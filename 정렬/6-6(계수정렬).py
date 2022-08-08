ce9cbff7a0b9191b256fe2dca88e2d59be468060
hajeehoon12 <57164835+hajeehoon12@users.noreply.github.com>
2022-08-08 15:14:49 +0900

    계수정렬

---
+++ 정렬/6-6(계수정렬).py
@@ -0,0 +1,12 @@
+# 계수를 특정하여 특정한 조건하에서만 사용가능
+
+array = [7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
+
+count = [0]*(max(array)+1)
+
+for i in range(len(array)):
+    count[array[i]] += 1
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i , end = ' ')
